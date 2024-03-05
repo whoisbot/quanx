a2c3cda087209dc1b9b9df5261ce35daeba4156e
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-05 23:09:21 +0800

    update

--- goodnotes6.js
+++ goodnotes6.js
@@ -21,8 +21,8 @@
 
 
 
-^http[s]?:\/\/isi\.csan\.goodnotesapp\.com\.cn\/v1\/subscribers\/[^/]+$ url script-echo-response https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes6.js
-^http[s]?:\/\/isi\.csan\.goodnotes\.com\/v1\/subscribers\/[^/]+$ url script-echo-response https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes6.js
+^http[s]?:\/\/isi\.csan\.goodnotesapp\.com\.cn\/v1\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes6.js
+^http[s]?:\/\/isi\.csan\.goodnotes\.com\/v1\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes6.js
 
 
 

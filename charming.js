1457f3a4188b549c9c12f44a86d4938d3155bec6
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-08 11:33:18 +0800

    charming.js

---
+++ charming.js
@@ -0,0 +1,22 @@
+/*
+
+Quantumult X 脚本
+charmjng数藏
+
+
+
+
+[rewrite_local]
+^http[s]?:\/\/api\.charmingapp\.cn\/charming\/p\/current\/milli\/time url script-response-body charming.js
+[mitm]
+
+hostname = api.revenuecat.com
+
+*/
+
+let obj = JSON.parse($response.body);
+obj.current_milli_time=1709869288000;
+
+
+$done({body: JSON.stringify(obj)});
+

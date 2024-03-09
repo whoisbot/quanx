15ff9a4c9b025af3a2c1f1a5f72b3f359f9cfc97
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-09 20:52:05 +0800

    update

--- goodnotes2.js
+++ goodnotes2.js
@@ -1,13 +1,9 @@
 // 假设脚本类型为 script-response-header，只修改响应头
-var newHeader = $response.headers;
-newHeader['Content-Encoding'] = 'gzip';
-newHeader['Connection'] = 'close';
-newHeader['Content-Length'] = '557'; // 假设响应体的长度是557字节
-
-// 创建新的响应对象
-var newResponse = {
-    status: $response.status, // 保留原始状态码和状态信息
-    headers: newHeader, // 使用新的头部
+var header = $response.headers;
+header={
+'Content-Encoding':'gzip',
+'Connection':'close',
+'Content-Length':'557' // 假设响应体的长度是557字节
 };
 
-$done(newResponse);
+$done({headers: header});

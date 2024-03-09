d3e6b6335765f91c3a6937fc43eee35a41368261
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-09 22:55:54 +0800

    update

--- goodnotes.js
+++ goodnotes.js
@@ -73,7 +73,7 @@ var headers = {
     // 'Content-Length'：在这里不设置，让服务器/客户端自动处理
 
 };
-    headers['Content-Length'] =Buffer.byteLength(body, 'utf8'); // 设置正确的长度
+    headers['Content-Length'] = Buffer.byteLength(body, 'utf8').toString();
 var response = {
     body: body,
     headers: headers

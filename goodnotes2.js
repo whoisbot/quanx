e881bb511070c0f6efc92516bab2f62ae331e166
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-09 21:46:40 +0800

    update

--- goodnotes2.js
+++ goodnotes2.js
@@ -23,12 +23,7 @@ hostname=isi.csan.goodnotes.com.cn, isi.csan.goodnotes.com,isi.csan.goodnotesapp
 
 
 let obj = JSON.parse($response.body);
-
-obj={
-    "request_date_ms":1691760087616,
-    "request_date":"2023-08-11T13:21:27Z",
-    "subscriber":{
-        "non_subscriptions":{
+obj.subscriber.non_subscriptions={
             "com.goodnotes6.one_time_unlock":[
                 {
                     "is_sandbox":false,
@@ -40,31 +35,23 @@ obj={
                     "store":"app_store"
                 }
             ]
-        },
-        "first_seen":"2023-08-09T13:55:38Z",
-        "original_application_version":"1578030.383984727",
-        "other_purchases":{
+        };
+obj.subscriber.subscriptions={};
+
+obj.subscriber.other_purchases={
             "com.goodnotes6.one_time_unlock":{
                 "purchase_date":"2023-08-10T23:58:09Z"
-            }
-        },
-        "management_url":null,
-        "subscriptions":{
-
-        },
-        "entitlements":{
+            };
+            
+obj.subscriber.entitlements={
             "apple_access":{
                 "grace_period_expires_date":null,
                 "purchase_date":"2023-08-10T14:16:41Z",
                 "product_identifier":"com.goodnotes6.one_time_unlock",
                 "expires_date":null
             }
-        },
-        "original_purchase_date":"2022-04-05T10:43:53Z",
-        "original_app_user_id":"1d6316b8-aab7-4c1f-9dee-df471814b03e",
-        "last_seen":"2023-08-11T13:18:22Z",
-		"Warning":"本脚本仅供学习交流使用，禁止转载售卖"
-    }
-};
+        };
+         
+
 
 $done({body: JSON.stringify(obj)});

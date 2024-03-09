a4696f1f1dc23b8d5903b957b716045635979e60
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-09 20:35:20 +0800

    update

--- goodnotes2.js
+++ goodnotes2.js
@@ -3,21 +3,12 @@
 
 
 
-var modifiedHeaders = $response.headers;
+var header = $response.headers;
 
 // 修改或添加所需的响应头
-modifiedHeaders['Content-Encoding'] = 'gzip';
-modifiedHeaders['Connection'] = 'close';
+header['Content-Encoding'] = 'gzip';
+header['Connection'] = 'close';
 
-
-// 创建新的响应对象，不包含对响应体的引用
-var newResponse = {
-    status: $response.status, // 保留原始的状态行
-    headers: modifiedHeaders // 使用修改后的响应头
-    // body 属性被省略，响应体不会被修改
-};
-
-$done(newResponse);
 $done({headers: header});
 
 

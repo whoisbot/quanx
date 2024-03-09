90f9c588bab19c0d724262542d9ac24142fac20a
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-09 21:27:18 +0800

    update

--- goodnotes.js
+++ goodnotes.js
@@ -12,7 +12,7 @@
 
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes(app)?\.com(\.cn)?(\/v1)?\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes.js
+^https:\/\/isi\.csan\.goodnotes(app)?\.com(\.cn)?(\/v1)?\/(receipts$|subscribers\/[^/]+$) url script-response-header https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes.js
 
 ^https:\/\/isi\.csan\.goodnotes\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
 
@@ -27,47 +27,6 @@ var header = $response.headers;
 header['Connection']='close';
 header['Content-Length']='557';
 
-var obj={
-    "request_date_ms":1691760087616,
-    "request_date":"2023-08-11T13:21:27Z",
-    "subscriber":{
-        "non_subscriptions":{
-            "com.goodnotes6.one_time_unlock":[
-                {
-                    "is_sandbox":false,
-                    "ownership_type":"PURCHASED",
-                    "id":"46ab585dbc",
-                    "original_purchase_date":"2022-11-10T23:58:09Z",
-                    "store_transaction_id":"300001271068792",
-                    "purchase_date":"2023-08-10T23:58:09Z",
-                    "store":"app_store"
-                }
-            ]
-        },
-        "first_seen":"2023-08-09T13:55:38Z",
-        "original_application_version":"1578030.383984727",
-        "other_purchases":{
-            "com.goodnotes6.one_time_unlock":{
-                "purchase_date":"2023-08-10T23:58:09Z"
-            }
-        },
-        "management_url":null,
-        "subscriptions":{
 
-        },
-        "entitlements":{
-            "apple_access":{
-                "grace_period_expires_date":null,
-                "purchase_date":"2023-08-10T14:16:41Z",
-                "product_identifier":"com.goodnotes6.one_time_unlock",
-                "expires_date":null
-            }
-        },
-        "original_purchase_date":"2022-04-05T10:43:53Z",
-        "original_app_user_id":"1d6316b8-aab7-4c1f-9dee-df471814b03e",
-        "last_seen":"2023-08-11T13:18:22Z",
-		"Warning":"本脚本仅供学习交流使用，禁止转载售卖"
-    }
-};
 
-$done({headers: header, body: JSON.stringify(obj)});
+$done({headers: header});

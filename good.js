5bfef09b4b47e5caf2fec332d7a20cd3fd2acaab
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-09 08:45:55 +0800

    update

--- good.js
+++ good.js
@@ -32,5 +32,5 @@ hostname=isi.csan.goodnotes.com.cn, isi.csan.goodnotes.com,isi.csan.goodnotesapp
 
 var header = $request.headers;
 header['X-RevenueCat-ETag'] = '8f1129098e39f74e';
-$done({ headers: header });
+$done({headers : header});
 

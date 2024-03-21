da7bb4f96d83b5e316bbfae15dcd260a654e609b
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-03-21 18:04:09 +0800

    update

--- goodnotes6.js
+++ goodnotes6.js
@@ -15,7 +15,7 @@
 ^https:\/\/isi\.csan\.goodnotes(app)?\.com(\.cn)?(\/v1)?\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/whoisbot/quanx/main/goodnotes6.js
 ^http[s]?:\/\/isi\.csan\.goodnotesapp\.com\.cn\/v1\/(receipts$|subscribers\/[^/]+$) url response-header 304 response-header 200
 
-^http[s]?:\/\/isi\.csan\.goodnotesapp\.com\.cn\/v1\/(receipts$|subscribers\/[^/]+$) url request-header X-RevenueCat-ETag：
+^http[s]?:\/\/isi\.csan\.goodnotesapp\.com\.cn\/v1\/(receipts$|subscribers\/[^/]+$) url request-header X-RevenueCat-ETag:
 
 [mitm] 
 

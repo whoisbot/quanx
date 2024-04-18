30f03ff8dc1431c183347bfb6e2984238e559efa
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-04-18 14:34:34 +0800

    update

--- xiugai.js
+++ xiugai.js
@@ -15,143 +15,27 @@ hostname=buy.vmall.com
 ***********************************/
 
 var obj = JSON.parse($response.body);
-var obj={
-  "success" : true,
-  "skuRushBuyInfoList" : [
-    {
-      "saleInventory" : null,
-      "placeholderDesc" : null,
-      "attendQualification" : 1,
-      "bankReserveList" : null,
-      "placeholder" : null,
-      "isTarget" : null,
-      "endTime" : 1713455999000,
-      "activityType" : "1",
-      "activityId" : 232517,
-      "sbomCode" : "2601010486503",
-      "skuStatus" : 1,
-      "groupTagNameList" : null,
-      "totalInventory" : null,
-      "activityName" : null,
-      "subscribeActivityCode" : null,
-      "groupId" : null,
-      "startTime" : 1713420366571,
-      "price" : null,
-      "isYY" : null,
-      "limitNum" : 1
-    },
-    {
-      "saleInventory" : null,
-      "placeholderDesc" : null,
-      "attendQualification" : 1,
-      "bankReserveList" : null,
-      "placeholder" : null,
-      "isTarget" : null,
-      "endTime" : 1713455999000,
-      "activityType" : "1",
-      "activityId" : 232517,
-      "sbomCode" : "2601010486504",
-      "skuStatus" : 1,
-      "groupTagNameList" : null,
-      "totalInventory" : null,
-      "activityName" : null,
-      "subscribeActivityCode" : null,
-      "groupId" : null,
-      "startTime" : 1713420366571,
-      "price" : null,
-      "isYY" : null,
-      "limitNum" : 1
-    },
-    {
-      "saleInventory" : null,
-      "placeholderDesc" : null,
-      "attendQualification" : 1,
-      "bankReserveList" : null,
-      "placeholder" : null,
-      "isTarget" : null,
-      "endTime" : 1713455999000,
-      "activityType" : "1",
-      "activityId" : 232517,
-      "sbomCode" : "2601010486505",
-      "skuStatus" : 1,
-      "groupTagNameList" : null,
-      "totalInventory" : null,
-      "activityName" : null,
-      "subscribeActivityCode" : null,
-      "groupId" : null,
-      "startTime" : 1713420366571,
-      "price" : null,
-      "isYY" : null,
-      "limitNum" : 1
-    },
-    {
-      "saleInventory" : null,
-      "placeholderDesc" : null,
-      "attendQualification" : 1,
-      "bankReserveList" : null,
-      "placeholder" : null,
-      "isTarget" : null,
-      "endTime" : 1713455999000,
-      "activityType" : "1",
-      "activityId" : 232517,
-      "sbomCode" : "2601010486506",
-      "skuStatus" : 1,
-      "groupTagNameList" : null,
-      "totalInventory" : null,
-      "activityName" : null,
-      "subscribeActivityCode" : null,
-      "groupId" : null,
-      "startTime" : 1713420366571,
-      "price" : null,
-      "isYY" : null,
-      "limitNum" : 1
-    },
-    {
-      "saleInventory" : null,
-      "placeholderDesc" : null,
-      "attendQualification" : 1,
-      "bankReserveList" : null,
-      "placeholder" : null,
-      "isTarget" : null,
-      "endTime" : 1713455999000,
-      "activityType" : "1",
-      "activityId" : 232517,
-      "sbomCode" : "2601010486501",
-      "skuStatus" : 1,
-      "groupTagNameList" : null,
-      "totalInventory" : null,
-      "activityName" : null,
-      "subscribeActivityCode" : null,
-      "groupId" : null,
-      "startTime" : 1713420366571,
-      "price" : null,
-      "isYY" : null,
-      "limitNum" : 1
-    },
-    {
-      "saleInventory" : null,
-      "placeholderDesc" : null,
-      "attendQualification" : 1,
-      "bankReserveList" : null,
-      "placeholder" : null,
-      "isTarget" : null,
-      "endTime" : 1713455999000,
-      "activityType" : "1",
-      "activityId" : 232517,
-      "sbomCode" : "2601010486502",
-      "skuStatus" : 1,
-      "groupTagNameList" : null,
-      "totalInventory" : null,
-      "activityName" : null,
-      "subscribeActivityCode" : null,
-      "groupId" : null,
-      "startTime" : 1713420366571,
-      "price" : null,
-      "isYY" : null,
-      "limitNum" : 1
-    }
-  ],
-  "currentTime" : 1713420366571
+// Quantumult X 脚本示例：修改 JSON 响应体中的 startTime
+
+// 获取响应体
+let body = $response.body;
+
+// 将 string 类型的响应体转换为 JSON 对象
+let obj = JSON.parse(body);
+
+// 检查 obj 是否有 skuRushBuyInfoList 属性且它是一个数组
+if (obj.skuRushBuyInfoList && Array.isArray(obj.skuRushBuyInfoList)) {
+    // 遍历数组，修改每个对象中的 startTime 属性
+    obj.skuRushBuyInfoList.forEach(function(item, index) {
+        item.startTime = 1713420366571; // 替换为您想要的时间戳
+    });
 }
 
-$done({body: JSON.stringify(obj)});
+// 将修改后的 JSON 对象转换回 string 类型的响应体
+body = JSON.stringify(obj);
+
+// 设置修改后的响应体
+$response.body = body;
+
+// 最后调用 $done() 方法完成脚本执行
+$done();

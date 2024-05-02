b9e1115463929b262527aa26e723e3d87646a23b
Fan <dlppycz@gmail.com>
2024-05-02 19:38:31 +0800

    update

--- xiugai.js
+++ xiugai.js
@@ -15,34 +15,29 @@ hostname=buy.vmall.com
 
 ***********************************/
 
-var obj = JSON.parse($response.body);
+var data = JSON.parse($response.body);
 // Quantumult X 脚本示例：修改 JSON 响应体中的 startTime
 
-// 获取响应体
-let body = $response.body;
 
-// 将 string 类型的响应体转换为 JSON 对象
-let obj = JSON.parse(body);
-
-// 递归函数，用于深度搜索所有的 startTime 并修改其值
-function updateStartTime(o) {
-    for (var key in o) {
-        if (typeof o[key] == 'object' && o[key] !== null) {
-            updateStartTime(o[key]);
-        } else if (key == 'startTime') {
-            o[key] = '1714649060000'; // 您想要的新 startTime 值
+function rewriteTime(obj) {
+    for (var prop in obj) {
+        if (typeof obj[prop] === 'object') {
+            rewriteTime(obj[prop]);
+        } else if (prop === 'startTime') {
+            // 设置新的 startTime 值，您可以根据需要修改这里的值
+            obj[prop] = '1714649666254';
         }
     }
 }
 
-// 调用函数并传入 JSON 对象
-updateStartTime(obj);
+rewriteTime(data);
 
 
-body = JSON.stringify(obj);
+
+data = JSON.stringify(data);
 
 // 设置修改后的响应体
-$response.body = body;
+$response.body = data;
 
 // 最后调用 $done() 方法完成脚本执行
 $done();

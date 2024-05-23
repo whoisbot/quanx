6d5224a5767f058f546d95e30b35b42a966042f3
Fan <dlppycz@gmail.com>
2024-05-23 19:10:38 +0800

    jlhx.js

--- jlhx.js
+++ jlhx.js
@@ -26,14 +26,14 @@ var url = $request.url;
 function modifyResponse(url, body) {
   if (url.indexOf('queryPage.do') > -1) {
   
-    body = {"ret":1,"total":1,"data":[{"buyTime":"2024-05-23","currentNum":0,"endDate":"2025-05-30","feeName":"全车型","isValid":true,"maxRide":999,"name":"9.9元999次卡","rideCardId":100847,"ridecardType":0,"startDate":"2024-05-23","timeCount":12,"updateTime":3155871162400,"useCount":0,"userId":9136658,"userType":0}]}
+    body = {"ret":1,"total":1,"data":[{"buyTime":"2024-05-23","currentNum":0,"endDate":"2025-05-30","feeName":"全车型","isValid":true,"maxRide":999,"name":"9.9元999次卡","rideCardId":100847,"ridecardType":0,"startDate":"2024-05-23","timeCount":120,"updateTime":3155871162400,"useCount":0,"userId":9136658,"userType":0}]}
   } else if (url.indexOf('getByMachineNO.do') > -1) {
  
     body.batteryEI=+1;
     body.times=3;
   }else if (url.indexOf('getByAccountId.do') > -1) {
    
-        body={"ret":1,"data":{"accountId":700137,"accountName":"哈尔滨华德学院","areaMoney":2000,"baseMile":0,"baseMileFee":0,"baseTime":12,"baseTimeFee":170,"capMile":0,"capMileFee":0,"capTime":0,"capTimeFee":0,"dispatchSwitch":1,"freeRideTime":2,"freeTime":1,"helmetMoney":0,"helmetSwitch":0,"newUserFreeTime":0,"newUserFreeTimes":0,"newUserRechcrge":0,"overMile":0,"overMileFee":0,"overTime":10,"overTimeFee":170,"parkPointMoney":300,"rechargeBase":0}}
+        body={"ret":1,"data":{"accountId":700137,"accountName":"哈尔滨华德学院","areaMoney":2000,"baseMile":0,"baseMileFee":0,"baseTime":120,"baseTimeFee":170,"capMile":0,"capMileFee":0,"capTime":0,"capTimeFee":0,"dispatchSwitch":1,"freeRideTime":2,"freeTime":120,"helmetMoney":0,"helmetSwitch":0,"newUserFreeTime":0,"newUserFreeTimes":0,"newUserRechcrge":0,"overMile":0,"overMileFee":0,"overTime":10,"overTimeFee":170,"parkPointMoney":300,"rechargeBase":0}}
   }else if (url.indexOf('getDiscountByMachineNO.do') > -1) {
    
     body={"rideCard":{"accountId":700137,"buyTime":"2024-05-23","currentNum":0,"endDate":"2025-05-30","feeId":0,"feeName":"全车型","isValid":true,"maxRide":999,"name":"9.9元999次卡","rideCardId":100847,"ridecardType":0,"startDate":"2024-05-23","timeCount":12,"updateTime":3155871162400,"useCount":0,"userId":9136658,"userType":0},"ret":1}

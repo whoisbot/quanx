5827444e73dbdcd34393f27738992944fab0f164
Fan <dlppycz@gmail.com>
2024-05-23 10:14:29 +0800

    update jlhx.js

--- jlhx.js
+++ jlhx.js
@@ -12,6 +12,8 @@ Quantumult X 脚本
 
 ^http[s]?:\/\/jlclient\.uqbike\.cn\/machineStatus\/getDiscountByMachineNO\.do url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/jlhx.js
 
+^http[s]?:\/\/jlclient\.uqbike\.cn\/user\/getByUserId\.do url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/jlhx.js
+
 
 [mitm]
 hostname = jlclient.uqbike.cn,
@@ -35,6 +37,9 @@ function modifyResponse(url, body) {
   }else if (url.indexOf('getDiscountByMachineNO.do') > -1) {
    
     body={"rideCard":{"accountId":700137,"buyTime":"2024-05-23","currentNum":0,"endDate":"2025-05-30","feeId":0,"feeName":"全车型","isValid":true,"maxRide":999,"name":"9.9元999次卡","rideCardId":100847,"ridecardType":0,"startDate":"2024-05-23","timeCount":12,"updateTime":3155871162400,"useCount":0,"userId":9136658,"userType":0},"ret":1}
+  }else if (url.indexOf('getByUserId.do') > -1) {
+   
+    body={"ret":1,"data":{"accountId":40179,"birthDay":"**********","depositMoney":0.0,"depositState":0,"email":"","idNO":"","joinTime":"2024-05-06 07:00:26","money":0.0,"name":"","nameAuth":false,"openId":"1","phone":"133****4620","remark":"","rideCardDate":"2025-05-30","rideMoney":0.0,"ridingScore":99999,"sex":"","studentAuth":false,"updateTime":"2025-05-23 08:05:12","userId":9136658}
   }
   
   return body;

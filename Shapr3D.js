10478f2962ed964019745ff1d32f3ee7c683ecdf
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-05-27 20:46:28 +0800

    update

--- Shapr3D.js
+++ Shapr3D.js
@@ -13,7 +13,7 @@ unlock Shapr:3D PRO
 
 [rewrite_local]
 # unlock Shapr3D
-^https?:\/\/prod\.api\.shapr3d\.com\/user-management\/profile-with-device url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
+^https?:\/\/prod\.api\.shapr3d\.com\/user-management\/teams-profile url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
 
 [mitm]
 hostname = prod.api.shapr3d.com,
@@ -21,16 +21,42 @@ hostname = prod.api.shapr3d.com,
 */
 
 let obj = JSON.parse($response.body);
-obj.subscriptionType = "pro_year";
-obj.subscriptionExpires = "2100-01-01T00:00:00.000Z";
-obj.subscriptions=[{
-    "id": 206992,
-    "type": "appstore",
+obj = {
+  "id": "b750213e-9b39-49ec-9679-009b97736e89",
+  "email": "445165653@qq.com",
+  "subscriptionType": "edu",
+  "subscriptions": [{
+    "id": 501413,
+    "type": "edu",
     "tier": "pro",
+    "effectiveFeatureSet": "pro",
     "period": "yearly",
-    "purchaseDate": "2021-03-15T13:45:16.000Z",
-    "expirationDate": "2029-03-29T13:45:16.000Z",
-    "autoRenewStatus": true,
+    "purchaseDate": "2022-04-21T03:01:15.302Z",
+    "expirationDate": "2099-12-31T03:01:15.302Z",
+    "autoRenewStatus": false,
     "isTrial": false
   }],
+  "subscriptionExpires": "2099-12-31T03:01:15.302Z",
+  "hasEmail": true,
+  "hasPassword": true,
+  "hasFacebookID": false,
+  "syncPermissions": {
+    "projectSyncAllowed": true,
+    "projectSyncFeatureFlag": true
+  },
+  "plans": {
+    "paddle.lite.monthly": {
+      "tier": "lite",
+      "period": "monthly",
+      "isTrialEligible": false,
+      "checkoutURL": "https://account.shapr3d.com/?period=monthly&tier=lite"
+    },
+    "paddle.lite.yearly": {
+      "tier": "lite",
+      "period": "yearly",
+      "isTrialEligible": false,
+      "checkoutURL": "https://account.shapr3d.com/?period=yearly&tier=lite"
+    }
+  }
+};
 $done({body: JSON.stringify(obj)});

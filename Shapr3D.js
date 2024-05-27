d82b7fc567ed05624544cdc4f14589b59848b696
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-05-27 21:26:22 +0800

    update

--- Shapr3D.js
+++ Shapr3D.js
@@ -14,7 +14,7 @@ unlock Shapr:3D PRO
 [rewrite_local]
 # unlock Shapr3D
 ^https?:\/\/prod\.api\.shapr3d\.com\/user-management\/profile-with-device url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
-
+^https?:\/\/prod\.api\.shapr3d\.com\/user-management\/account url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
 [mitm]
 hostname = prod.api.shapr3d.com,
 

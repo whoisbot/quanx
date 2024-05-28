882710637c396cac586d53f6c7a13fe06efc3120
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-05-28 08:35:11 +0800

    update

--- Shapr3D.js
+++ Shapr3D.js
@@ -5,32 +5,42 @@ unlock Shapr:3D PRO
 > 应用名称：Shapr3D
 > 脚本作者：whoisbot
 > 解锁说明：解锁高级会员权限
-> 更新时间：2024-03-08
+> 更新时间：2024-05-28
 > 特别提醒：如需转载请注明出处，谢谢合作！
 > 特别说明：⚠️⚠️⚠️
           本脚本仅供学习交流使用，禁止转载售卖
           ⚠️⚠️⚠️
-^https?:\/\/prod\.api\.shapr3d\.com\/user-management\/profile-with-device url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
+
 [rewrite_local]
 # unlock Shapr3D
 
-^https?:\/\/prod\.api\.shapr3d\.com\/user-management\/account url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
+^http[s]?:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/Shapr3D.js
 [mitm]
-hostname = prod.api.shapr3d.com,
+hostname = buy.itunes.apple.com,
 
 */
 
 let obj = JSON.parse($response.body);
-obj.subscriptionType = "pro_year";
-obj.subscriptionExpires = "2100-01-01T00:00:00.000Z";
-obj.subscriptions=[{
-    "id": 206992,
-    "type": "appstore",
-    "tier": "pro",
-    "period": "yearly",
-    "purchaseDate": "2021-03-15T13:45:16.000Z",
-    "expirationDate": "2029-03-29T13:45:16.000Z",
-    "autoRenewStatus": true,
-    "isTrial": false
-  }],
+obj.status = "1";
+
+obj.pending_renewal_info=[{
+    "expiration_intent": "0",
+    "auto_renew_product_id": "com.shapr3d.shapr.iap.lite2.renewing.yearly",
+    "is_in_billing_retry_period": "0",
+    "product_id": "com.shapr3d.shapr.iap.lite2.renewing.yearly",
+    "original_transaction_id": "510000657302855",
+    "auto_renew_status": "1"
+  }];
+
+obj.receipt.in_app[0].expires_date="2099-03-29 13:45:16 Etc/GMT";
+obj.receipt.in_app[0].expires_date_ms="4077265517000";
+obj.receipt.in_app[0].expires_date_pst="2099-03-29 06:45:16 America/Los_Angeles";
+obj.receipt.in_app[0].is_trial_period="false";
+obj.receipt.in_app[0].is_in_intro_offer_period"="true";
+obj.latest_receipt_info[0].expires_date="2099-03-29 13:45:16 Etc/GMT";
+obj.latest_receipt_info[0].expires_date_ms="4077265517000";
+obj.latest_receipt_info[0].expires_date_pst="2099-03-29 06:45:16 America/Los_Angeles";
+obj.latest_receipt_info[0].is_trial_period="false";
+obj.latest_receipt_info[0].is_in_intro_offer_period"="true";
+
 $done({body: JSON.stringify(obj)});

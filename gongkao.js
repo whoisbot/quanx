0298fe672eaf2921463f2da59548e3b85b0bcc13
whoisbot <67591209+whoisbot@users.noreply.github.com>
2024-12-29 05:18:45 +0800

    gongkao.js

--- gongkao.js
+++ gongkao.js
@@ -7,6 +7,7 @@ Quantumult X 脚本
 [rewrite_local]
 
 ^http[s]?:\/\/api\.gongkaoleida\.com\/api\/v1\/intelligent\/checkIntelligent url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/gongkao.js
+^http[s]?:\/\/api\.gongkaoleida\.com\/api\/v5_4_8\/user\/resume url script-response-body https://raw.githubusercontent.com/whoisbot/quanx/main/gongkao.js
 
 
 
@@ -22,6 +23,14 @@ function modifyResponse(url, body) {
   if (url.indexOf('checkIntelligent') > -1) {
   body.data.isStarVip=true;
   
+  } 
+  else if (url.indexOf('consume') > -1) {
+  body.data.userInfo.vipExpire="999999";
+
+  body.data.userInfo.isVip="1";
+  body.data.userInfo.vipGradeList[1].isVip="1";
+  body.data.userInfo.vipGradeList[1].remainDays="9999999";
+  body.data.userInfo.vipGradeList[1].vipExpire="9999999";
   } 
   
   return body;
